dff67ee3213cd4fa4dc2eedb49b6ce7efa6eb53d
ObaidUr-Rehman <obaidurrehman8121@gmail.com>
2023-12-24 15:30:44 +0500

    development

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from './Pages/Home/Home';
 function App() {
   return (
     <h1 className="text-3xl font-bold underline mt-12">
-      Hello world...
+      Hello world!!
       <Home />
     </h1>
   );
