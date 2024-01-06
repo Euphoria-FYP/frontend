618dd9b075ba466676f6e6184cc7cc65623bf6f8
ObaidUr-Rehman <obaidurrehman8121@gmail.com>
2024-01-06 17:43:15 +0500

    code in process

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,7 @@ import Home from './Pages/Home/Home';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline mt-12">
-      Hello world...
-      <Home />
-    </h1>
+    <Home />
   );
 }
 
