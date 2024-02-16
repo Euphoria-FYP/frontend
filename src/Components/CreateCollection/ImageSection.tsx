383f7d3db54447109805a579e667f15c30238430
MuhammadDaniyal <daniyalshiekh166@gmail.com>
2024-02-17 01:06:24 +0500

    images work done

--- src/Components/CreateCollection/ImageSection.tsx
+++ src/Components/CreateCollection/ImageSection.tsx
@@ -1,17 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoMdImage } from "react-icons/io";
 import { FiUpload } from "react-icons/fi";
 
 const ImageSection = () => {
+  const [logoFile, setlogoFile] = useState<File | null>(null);
+  const [logoPreview, setlogoPreview] = useState<string | null>(null);
+  
+  const [coverFile, setcoverFile] = useState<File | null>(null);
+  const [coverPreview, setcoverPreview] = useState<string | null>(null);
+
+  const handleLogoSelect = (event: any) => {
+    const file = event.target.files && event.target.files[0];
+    if (file) {
+      setlogoFile(file);
+      // Read the selected file and set image preview
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        if (typeof reader.result === "string") {
+          setlogoPreview(reader.result);
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleCoverSelect = (event: any) => {
+    const file = event.target.files && event.target.files[0];
+    if (file) {
+      setcoverFile(file);
+      // Read the selected file and set image preview
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        if (typeof reader.result === "string") {
+          setcoverPreview(reader.result);
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  };
   return (
     <>
-      <div className=" flex gap-3">
+      <div
+        className=" flex gap-3"
+      >
         {/* logo upload div */}
-        <div className=" flex-1">
+        <div className=" flex-1" onClick={() => {
+          document.getElementById("uploadlogo")?.click();
+        }}>
           <label className="text-base font-semibold">Logo Image</label>
           {/* upload */}
-          <div className=" flex gap-5 mt-3 mb-5 border border-gray-500 px-2 py-3 rounded-lg group transition-all hover:border-gray-200 cursor-pointer">
+          <div
+            className={` ${
+              logoPreview && "hidden"
+            } flex gap-5 mt-3 mb-5 border border-gray-500 px-2 py-3 rounded-lg group transition-all hover:border-gray-200 cursor-pointer`}
+          >
             {/* icon */}
+            <input
+              type="file"
+              className=" hidden"
+              id="uploadlogo"
+              onChange={handleLogoSelect}
+            />
             <div className=" flex-1 flex justify-center items-center border border-dashed border-level-1 transition-all group-hover:border-solid w-full h-20 rounded-md">
               <IoMdImage className="block group-hover:hidden text-2xl" />
               <FiUpload className="hidden group-hover:block text-2xl" />
@@ -28,16 +77,36 @@ const ImageSection = () => {
               </span>
             </div>
           </div>
+          {logoPreview && (
+            <img
+              src={logoPreview}
+              alt="Preview"
+              style={{ objectFit: "contain" }}
+            />
+          )}
         </div>
 
         {/* cover image upload div */}
-        <div className=" flex-1">
-          <label className="text-base font-semibold">
-            Cover Image
-          </label>
+        <div
+          className=" flex-1"
+          onClick={() => {
+            document.getElementById("uploadcover")?.click();
+          }}
+        >
+          <label className="text-base font-semibold">Cover Image</label>
           {/* upload */}
-          <div className=" flex gap-5 mt-3 mb-5 border border-gray-500 px-2 py-3 rounded-lg group transition-all hover:border-gray-200 cursor-pointer">
+          <div
+            className={` ${
+              coverPreview && "hidden"
+            } flex gap-5 mt-3 mb-5 border border-gray-500 px-2 py-3 rounded-lg group transition-all hover:border-gray-200 cursor-pointer`}
+          >
             {/* icon */}
+            <input
+              type="file"
+              className=" hidden"
+              id="uploadcover"
+              onChange={handleCoverSelect}
+            />
             <div className=" flex-1 flex justify-center items-center border border-dashed border-level-1 transition-all group-hover:border-solid w-full h-20 rounded-md">
               <IoMdImage className="block group-hover:hidden text-2xl" />
               <FiUpload className="hidden group-hover:block text-2xl" />
@@ -54,6 +123,13 @@ const ImageSection = () => {
               </span>
             </div>
           </div>
+          {coverPreview && (
+            <img
+              src={coverPreview}
+              alt="Preview"
+              style={{ objectFit: "contain" }}
+            />
+          )}
         </div>
       </div>
     </>
--- src/Components/UploadNFT/UploadNFT.tsx
+++ src/Components/UploadNFT/UploadNFT.tsx
@@ -1,34 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import UploadForm from "./UploadForm";
 import { FiUpload } from "react-icons/fi";
 
 const UploadNFT = () => {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [imagePreview, setImagePreview] = useState<string | null>(null);
+
+  const handleFileSelect = (event: any) => {
+    const file = event.target.files && event.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+      // Read the selected file and set image preview
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        if (typeof reader.result === "string") {
+          setImagePreview(reader.result);
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <section className=" w-[80%] h-full flex justify-center gap-12 text-white mx-auto py-10">
       <div className=" w-[45%] flex flex-col">
         <div>
-          <h2 className="text-white mb-2 font-semibold text-base">Upload File</h2>
-          <p className=" text-[15px] font-light text-gray-400">Drag or choose your file to upload</p>
+          <h2 className="text-white mb-2 font-semibold text-base">
+            Upload File
+          </h2>
+          <p className=" text-[15px] font-light text-gray-400">
+            Drag or choose your file to upload
+          </p>
         </div>
 
         <div
           className=" border-[16px] border-slate-900 bg-slate-900 rounded-lg h-fit mt-9 cursor-pointer
           transition-all duration-300 ease-in-out transform-gpu hover:scale-105 hover:border-[12px] hover:border-[#0077ff]"
+          onClick={() => {
+            document.getElementById("uploadfile")?.click();
+          }}
         >
-          <div className=" flex gap-5 mt-3 mb-5 px-5 py-5">
+          <div
+            className={`flex gap-5 ${
+              imagePreview ? "" : "mt-5 mb-5 py-5"
+            } px-5`}
+          >
+            <input
+              type="file"
+              className="hidden"
+              id="uploadfile"
+              onChange={handleFileSelect}
+            />
             {/* icon */}
-            <div className=" flex-1 flex flex-col justify-center items-center border border-dashed border-level-1 transition-all group-hover:border-solid w-16 h-72 rounded-lg">
+            <div
+              className={` ${
+                imagePreview && "hidden"
+              } flex-1 flex flex-col justify-center items-center border border-dashed border-level-1 transition-all group-hover:border-solid w-16 h-72 rounded-lg`}
+            >
               <FiUpload className="text-2xl mb-3" />
               <h1 className=" text-lg">Drag and drop media</h1>
               <p className=" text-gray-400 text-sm mt-2">Max size: 50MB</p>
               <p className=" text-gray-400 text-sm">JPG, PNG, GIF, SVG, MP4</p>
             </div>
           </div>
+          {imagePreview && (
+            <img
+              src={imagePreview}
+              alt="Preview"
+              style={{ objectFit: "contain" }}
+            />
+          )}
         </div>
 
         <div>
-          <h1 className="text-white font-semibold text-base mb-2 mt-10">Note:</h1>
-          <p className=" text-[15px] font-light text-gray-400">Service Fee: 2.5%</p>
+          <h1 className="text-white font-semibold text-base mb-2 mt-10">
+            Note:
+          </h1>
+          <p className=" text-[15px] font-light text-gray-400">
+            Service Fee: 2.5%
+          </p>
         </div>
       </div>
 
